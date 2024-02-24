ef436fe234b55d9c0ef880e33ec871394c0b3f78
bisrat <bisratx@gmail.com>
2024-02-24 06:55:49 +0300

    project and about me navigation and navbar casestudy removed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,30 @@
-import Hero 		from '../components/sections/index/hero'
-import Looking 		from '../components/sections/index/looking'
-import About 		from '../components/sections/index/about'
-import Technical 	from '../components/sections/index/technical'
-import Career 		from '../components/sections/index/career'
-import FeaturedProjects	from '../components/sections/projects/featured'
+import Hero from "../components/sections/index/hero";
+import Looking from "../components/sections/index/looking";
+import About from "../components/sections/index/about";
+import Technical from "../components/sections/index/technical";
+import Career from "../components/sections/index/career";
+import FeaturedProjects from "../components/sections/projects/featured";
 
-import Color 		from '../components/utils/page.colors.util'
+import Color from "../components/utils/page.colors.util";
 
-import colors 		from '../content/index/_colors.json'
+import colors from "../content/index/_colors.json";
 
 //
 export default function HomePage() {
+  return (
+    <>
+      <Color colors={colors} />
+      <Hero />
+      {/* <Looking /> */}
+      <div id="projects">
+        <FeaturedProjects />
+      </div>
+      <div id="about-me">
+        <About />
+      </div>
 
-	return (
-		<>
-			<Color colors={colors} />
-			<Hero />
-			{/* <Looking /> */}
-			<FeaturedProjects />
-			<About />
-			<Technical />
-			{/* <Career /> */}
-		</>
-	);
+      <Technical />
+      {/* <Career /> */}
+    </>
+  );
 }
